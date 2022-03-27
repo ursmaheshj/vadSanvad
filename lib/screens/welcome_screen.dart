a91255cdbc75ce4bf3fef70229751370761f7004
Mahesh Jadhav <omkar28399@gmail.com>
2022-03-27 10:10:10 +0530

    ColorTween Animation applied

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -20,17 +20,11 @@ class _WelcomeScreenState extends State<WelcomeScreen>
     super.initState();
     controller = AnimationController(
       vsync: this,
-      duration: const Duration(seconds: 1),
+      duration: const Duration(seconds: 5),
     );
-    animation = CurvedAnimation(parent: controller, curve: Curves.bounceInOut);
+    animation =
+        ColorTween(begin: Colors.red, end: Colors.blue).animate(controller);
     controller.forward();
-    animation.addStatusListener((status) {
-      if (status == AnimationStatus.completed) {
-        controller.reverse(from: 1.0);
-      } else if (status == AnimationStatus.dismissed) {
-        controller.forward();
-      }
-    });
     controller.addListener(() {
       setState(() {});
       print(animation.value);
@@ -46,7 +40,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
+      backgroundColor: animation.value,
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -60,7 +54,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
                   // ignore: sized_box_for_whitespace
                   child: Container(
                     child: Image.asset('images/logo.png'),
-                    height: animation.value * 100,
+                    height: 50,
                   ),
                 ),
                 const Text(
